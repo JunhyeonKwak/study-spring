cee98d842d07f55a915b9892f1f315a844db4345
JunhyeonKwak <jhkwak905@naver.com>
2023-12-18 12:21:39 +0900

    .

--- src/main/java/study/studyspring/service/MemberService.java
+++ src/main/java/study/studyspring/service/MemberService.java
@@ -1,5 +1,7 @@
 package study.studyspring.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import study.studyspring.domain.Member;
 import study.studyspring.repository.MemberRepository;
 import study.studyspring.repository.MemoryMemberRepository;
@@ -7,10 +9,12 @@ import study.studyspring.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
